dddab1d374da477ce98813a2aaf3317b811c4781
Wayde B <wbstwayde@gmail.com>
2021-12-11 16:50:36 +1100

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to waydes test grounds, currently the system time is", time.Now())
+}
---
+++ randint.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("the best number is", rand.Intn(100))
+	fmt.Printf("bigger issues; %g\n", math.Sqrt(10))
+	fmt.Println(math.Pi)
+}
+
